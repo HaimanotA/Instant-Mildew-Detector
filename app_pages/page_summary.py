200885014b006fcac3ce98a79e9380ff5ad01db8
HaimanotA <haimiayana530@gmail.com>
2022-10-02 08:48:31 +0000

    Add project summary on 'page_summary.py' file.

--- app_pages/page_summary.py
+++ app_pages/page_summary.py
@@ -8,19 +8,20 @@ def page_summary_body():
 
     st.info(
         f"**General Information**\n"
-        f"* Mildew is a form of fungus. It can grow on any organic matter and is caused by humidity and moisture "
-        f"Anopheles mosquitoes.\n"
-        f"* A blood smear sample is collected, mixed with a reagent and examined in "
-        f"the microscope. Visual criteria are used to detect malaria parasites.\n"
-        f"* According to [WHO](https://www.who.int/news-room/fact-sheets/detail/malaria), "
-        f"in 2019, there were an estimated  229 million cases of malaria worldwide and an "
-        f"estimated 409 thousand deaths due to this diseease. "
-        f"Children <5 years are the most vulnerable group, accounting for 67% (274 thousand) "
+        f"* Powdery mildew is a fungal disease that affects a  wide range of plants. 
+        It can grow on any organic matter and is caused by humidity and moisture "
+        f"\n Powdery mildew diseases are caused by many different species of fungi in the order "
+        f"* Erysiphales."
+        f"\n Powdery mildew is one of thte easier plant disease to identify, as its symptoms are quite distinctive."
+        f"* According to [Wikipedia](https://www.en.m.wikipedia.org), "
+        f"Infected plants display white powdery spots on the leaves and stems"
+        f"e. "
+        f"The lower leaves are tthe most affected, but the mildew can appear on any above-ground part of the plant."
         f"of all malaria deaths worldwide in 2019.\n\n"
         f"**Project Dataset**\n"
-        f"* The available dataset contains 5643 out of +27 thousand images taken from "
-        f"blood smear workflow (when a drop of blood it taken on a glass slide) of cells that "
-        f"are parasitized or uninfected with malaria.")
+        f"* The available dataset contains +4000 out of +27 thousand images taken from "
+        f""
+        f".")
 
     st.write(
         f"* For additional information, please visit and **read** the "
@@ -30,6 +31,6 @@ def page_summary_body():
     st.success(
         f"The project has 2 business requirements:\n"
         f"* 1 - The client is interested in having a study to differentiate "
-        f"a parasitized and uninfected cell visually.\n"
-        f"* 2 - The client is interested to tell whether a given cell contains malaria parasite or not. "
+        f"a cherry leaf which contains a powdery mildew or a healthy one visually.\n"
+        f"* 2 - The client is interested to tell whether a given leaf contains powdery mildew or not. "
         )
